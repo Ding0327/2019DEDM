d2dec90aa985eb0c42e838139a782266068a70d5
Ding0327 <43614505+Ding0327@users.noreply.github.com>
2019-12-09 14:10:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function(){
+	$('#BackTop').click(function(){ 
+		$('html,body').animate({scrollTop:0}, 333);
+	});
+	$(window).scroll(function() {
+		if ( $(this).scrollTop() > 300 ){
+			$('#BackTop').fadeIn(222);
+		} else {
+			$('#BackTop').stop().fadeOut(222);
+		}
+	}).scroll();
+});
+
+//緩慢滑動
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
